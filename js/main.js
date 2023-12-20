e63633edf340bddeec1b1fe468cd469c164218dd
nurcancek <149467399+nurcancek@users.noreply.github.com>
2023-12-20 16:05:48 +0300

    data ve modül
    proje data ve javascript modül sistemi eklendi

---
+++ js/main.js
@@ -0,0 +1,14 @@
+import headerFunc from "./header.js";
+import productsFunc from "./products.js";
+
+//! add product to localStorage
+async function getData() {
+  const photos = await fetch("../js/data.json");
+  const data = await photos.json();
+
+  data ? localStorage.setItem("products", JSON.stringify(data)) : [];
+}
+
+getData();
+
+const products = localStorage.getItem("products");
